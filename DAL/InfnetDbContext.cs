08eb2615ff47f25c597be2dadb992d3c75200c66
leandraassis <assisleandra13@gmail.com>
2024-03-18 23:45:42 -0300

    tp3c-sharp

---
+++ DAL/InfnetDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using GestaoDeLaboratorios.Models;
+namespace GestaoDeLaboratorios.DAL
+{
+    public class InfnetDbContext : DbContext
+    {
+        public InfnetDbContext(DbContextOptions options) : base (options)
+        {
+            
+        }
+        public DbSet<Computadores> Computadores { get; set; }
+
+    }
+}
---
+++ Models/Computadores.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GestaoDeLaboratorios.Models
+{
+    public class Computadores
+    {
+        [Required]
+        [Key]
+        public int Id { get; set; }
+        public string Marca { get; set; }
+        public string Processador { get; set; }
+        public string PlacaMae { get; set; }
+        public string Memoria { get; set; }
+        public string HD { get; set; }
+        public float NumeroPratrimonio { get; set; }
+        public string DataDeCompra { get; set; }
+
+    }
+}
